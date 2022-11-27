d04a0637473644d8061bca8febc18c1b399ce803
Sulav-Don631 <Sulavshrestha631@gmail.com>
2022-11-27 21:25:43 +0545

    deleted

--- for i in range(0,100,2).py
+++
@@ -1,9 +0,0 @@
-username=input('Name: '.upper())
-password=input('Password: '.upper())
-if int(len(password))==4:
-	print('Your password is incorrect'.upper())
-	print('Your not the user')
-else:
-	print(f'Name : {username}'.upper())
-	print(f'passwprd : {passwprd}'.lower())
-
--- sulav.py
+++
@@ -1,6 +0,0 @@
-is_old = True
-is_liscence= True
-
-if is_old:
-	print("you are old enough to drive")
-print("check check")
